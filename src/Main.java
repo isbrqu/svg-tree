d2426da8d9fe688398eefe1b9fda71b5d39352f3
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-01-19 13:50:57 -0300

    elimina cosas innecesarias del Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
@@ -7,29 +6,19 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import java.lang.Math;
-import java.util.HashMap;
-import java.util.Arrays;
-import svgtree.TagCreator;
-import svgtree.Proportion;
 import svgtree.Tree;
 
 public class Main {
 
-  private static Document doc;
-  private static Element svg;
-  private static Element tree;
-  private static final String SVG = "http://www.w3.org/2000/svg";
   private static final String output = "out/tree.svg";
-
   public static final int HEIGHT = 3;
 
   public static void
   main(String[] args) {
     try {
       Tree tree = new Tree(HEIGHT);
-      doc = tree.getDocument();
-      tree.drawTree(HEIGHT, Proportion.RADIO);
+      tree.drawTree(HEIGHT, .5f);
+      Document doc = tree.getDocument();
       Transformer transformer = TransformerFactory
         .newInstance()
         .newTransformer();
